51934334cb0e77bd294b11c4e4e2115756b821ef
karishmasultan <160702579+karishmasultan@users.noreply.github.com>
2025-08-26 07:18:43 +0530

    code commit

---
+++ Javabasics/src/com/java/Javabasics/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.Javabasics;
+
+/**
+ * @author Dell 5470
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
